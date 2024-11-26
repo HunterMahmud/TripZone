b2b36c5f0cf0dd8092c6cdf7e573ee96d1d3c19e
huntermahmud <mahmudthedatahunter@gmail.com>
2024-11-26 23:07:52 +0600

    check: custom fonts and fonts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
-
-const Home:React.FC = () => {
-    return (
-        <div>
-            home page
-        </div>
-    );
+const Home: React.FC = () => {
+  return (
+    <div>
+      <div className="font-popins text-primary text-3xl">home page with popins and primary color</div>
+      <div className="font-barlow text-secondary text-2xl">this is barlow fonts with secondary</div>
+    </div>
+  );
 };
 
 export default Home;
