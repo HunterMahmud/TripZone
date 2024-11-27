d16917cdda4c6cc1e061ae52b8f789578f6d63de
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2024-11-27 11:32:00 +0600

    added: contact page

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import Home from "../pages/Home";
+import Contact from "../pages/Contact/Contact";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
         {
             path:'/',
             element: <Home/>
+        },
+        {
+          path:"/contact",
+          element: <Contact></Contact>
         }
     ]
   },
