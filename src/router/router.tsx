db3ad2e0e7040347704e7241a07673f5dc995858
Asaduzzaman70 <asdudjamanhisam@gmail.com>
2024-11-27 09:18:55 +0600

    Add: Blog page file

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,16 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import Home from "../pages/Home";
+import BlogLayout from "../pages/Blog/BlogLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayout/>,
-    children:[
-        {
-            path:'/',
-            element: <Home/>
-        }
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/blog',
+        element: <BlogLayout />
+      }
     ]
   },
 ]);
