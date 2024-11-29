759a4fce50443bb4b8c956aa8c3ac4f8a1255400
huntermahmud <mahmudthedatahunter@gmail.com>
2024-11-29 19:07:16 +0600

    add: base url added from .env.local

--- src/hooks/useAxiosPublic.tsx
+++ src/hooks/useAxiosPublic.tsx
@@ -2,7 +2,7 @@
 import axios  from 'axios';
 
 const axiosPublic = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: import.meta.env.VITE_BASE_URL,
     // baseURL: 'https://trip-zone-server.vercel.app',
 })
 const useAxiosPublic = () => {
