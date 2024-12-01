192cc2fafba510e78f92c2386b8f5303ec942b7e
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2024-12-01 11:05:23 +0600

    why choose us section added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Packages from '../components/Packages';
+import WhyChooseUs from '../components/WhyChooseUs';
 import Banner from './../components/Home/Banner';
 const Home: React.FC = () => {
   return (
     <div>
       <Banner />
       <Packages/>
+      <WhyChooseUs/>
     </div>
   );
 };
